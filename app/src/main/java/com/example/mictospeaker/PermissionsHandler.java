e5f9344da279e32be536930052207a2637281289
Hakkeo <mellowinrain@gmail.com>
2025-04-02 19:19:23 +0545

    Update PermissionsHandler.java

--- app/src/main/java/com/example/mictospeaker/PermissionsHandler.java
+++ app/src/main/java/com/example/mictospeaker/PermissionsHandler.java
@@ -1,1 +1,23 @@
 
+package com.example.mictospeaker;
+
+import android.content.pm.PackageManager;
+import android.os.Build;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class PermissionsHandler {
+
+    public static boolean checkPermissions(AppCompatActivity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(activity, android.Manifest.permission.RECORD_AUDIO)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, new String[]{android.Manifest.permission.RECORD_AUDIO}, 1);
+                return false;
+            }
+        }
+        return true;
+    }
+}
