eee9c2fddbda1d798cddbbb796149dc89337550c
Hakkeo <mellowinrain@gmail.com>
2025-04-02 19:18:50 +0545

    Update AudioHandler.java

--- app/src/main/java/com/example/mictospeaker/AudioHandler.java
+++ app/src/main/java/com/example/mictospeaker/AudioHandler.java
@@ -1,1 +1,61 @@
 
+package com.example.mictospeaker;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
+
+public class AudioHandler {
+
+    private AudioRecord audioRecord;
+    private AudioTrack audioTrack;
+    private Thread audioThread;
+    private boolean isRecording = false;
+
+    public void start() {
+        isRecording = true;
+        int sampleRate = 44100;
+        int bufferSize = AudioRecord.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        
+        audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+        audioTrack = new AudioTrack.Builder()
+            .setAudioFormat(new android.media.AudioFormat.Builder()
+                .setSampleRate(sampleRate)
+                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
+                .build())
+            .setBufferSizeInBytes(bufferSize)
+            .build();
+
+        audioThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                byte[] buffer = new byte[bufferSize];
+                audioRecord.startRecording();
+                audioTrack.play();
+                
+                while (isRecording) {
+                    int read = audioRecord.read(buffer, 0, buffer.length);
+                    if (read > 0) {
+                        audioTrack.write(buffer, 0, read);
+                    }
+                }
+            }
+        });
+
+        audioThread.start();
+    }
+
+    public void stop() {
+        isRecording = false;
+        if (audioRecord != null) {
+            audioRecord.stop();
+            audioRecord.release();
+        }
+        if (audioTrack != null) {
+            audioTrack.stop();
+            audioTrack.release();
+        }
+    }
+}
